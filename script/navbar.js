0fa0247a8ee9ccddad2c4a316e0ed672e570f107
pldweb <paldiengineer@yahoo.com>
2022-02-06 12:10:38 +0700

    Commit Pertama

---
+++ script/navbar.js
@@ -0,0 +1,77 @@
+
+// $(document).ready(function() {
+//   $(window).scroll(function () {
+//     if ($(this).scrollTop() > 1) {
+//       $("custom-navbar").addClass("sticky");
+//     } else {
+//       $("custom-navbar").removeClass("sticky");
+//     }
+//   });
+// });
+
+
+
+window.onscroll = function() {
+  stickyFunction();
+}
+
+
+var navbar = document.getElementById("navbar-section");
+var sticky = navbar.offsetTop;
+
+
+function stickyFunction() {
+  if (window.pageYOffset > 0) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+
+// swiper 
+
+const swiper = new Swiper('.swiper', {
+  direction: 'horizontal',
+  loop:true,
+
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+
+});
+
+const cardSlider = new Swiper(".testi", {
+  autoplay: {
+    delay: 3000,
+  },
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  slidesPerView: 1,
+  spaceBetween: 70,
+  breakpoints: {
+
+   576: {
+     slidesPerView: 1,
+   } ,
+    // when window is <= 767px
+  768: {
+    slidesPerView: 1,
+    spaceBetween: 0,
+  },
+    // when window is <= 991px
+    991: {
+      slidesPerView: 3,
+      spaceBetween: 40,
+    },
+  },
+});
